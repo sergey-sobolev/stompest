d988438e9041dab668cf212b68a55be0f20c6c63
Jan Müller <jpmueller@gmx.net>
2012-11-22 23:12:35 +0100

    default broker ActiveMQ

--- stompest/tests/__init__.py
+++ stompest/tests/__init__.py
@@ -2,7 +2,7 @@ HOST = 'localhost'
 PORT = 61613
 VERSION = '1.1'
 
-BROKER = 'apollo'
+BROKER = 'activemq'
 LOGIN, PASSCODE, VIRTUALHOST = {
     'activemq': ('', '', ''),
     'apollo': ('admin', 'password', 'mybroker'),
