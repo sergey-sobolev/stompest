ce08922bafb404970074c40a58ad9b403580959e
Jan Müller <jpmueller@gmx.net>
2013-03-04 23:12:33 +0100

    Made disconnect reason private.

--- src/async/stompest/async/listener.py
+++ src/async/stompest/async/listener.py
@@ -83,21 +83,21 @@ class DisconnectListener(Listener):
 
     def onConnect(self, connection, frame): # @UnusedVariable
         self._disconnecting = False
-        self.disconnectReason = None
+        self._disconnectReason = None
         connection._disconnected = defer.Deferred()
 
     def onConnectionLost(self, connection, reason): # @UnusedVariable
         self.log.info('Disconnected: %s' % reason.getErrorMessage())
         if not self._disconnecting:
-            self.disconnectReason = StompConnectionError('Unexpected connection loss [%s]' % reason.getErrorMessage())
+            self._disconnectReason = StompConnectionError('Unexpected connection loss [%s]' % reason.getErrorMessage())
 
     def onCleanup(self, connection):
-        connection.session.close(flush=not self.disconnectReason)
+        connection.session.close(flush=not self._disconnectReason)
         connection.remove(self)
 
-        if self.disconnectReason:
-            # self.log.debug('Calling disconnected errback: %s' % self.disconnectReason)
-            connection._disconnected.errback(self.disconnectReason)
+        if self._disconnectReason:
+            # self.log.debug('Calling disconnected errback: %s' % self._disconnectReason)
+            connection._disconnected.errback(self._disconnectReason)
         else:
             # self.log.debug('Calling disconnected callback')
             connection._disconnected.callback(None)
@@ -105,7 +105,7 @@ class DisconnectListener(Listener):
 
     def onDisconnect(self, connection, failure, timeout): # @UnusedVariable
         if failure:
-            self.disconnectReason = failure
+            self._disconnectReason = failure
         if self._disconnecting:
             return
         self._disconnecting = True
@@ -118,15 +118,15 @@ class DisconnectListener(Listener):
         connection.nack(frame).addBoth(lambda _: None)
 
     @property
-    def disconnectReason(self):
-        return self._disconnectReason
+    def _disconnectReason(self):
+        return self.__disconnectReason
 
-    @disconnectReason.setter
-    def disconnectReason(self, reason):
+    @_disconnectReason.setter
+    def _disconnectReason(self, reason):
         if reason:
             self.log.error('Disconnect failure: %s' % reason)
-            reason = self.disconnectReason or reason # existing reason wins
-        self._disconnectReason = reason
+            reason = self._disconnectReason or reason # existing reason wins
+        self.__disconnectReason = reason
 
 class ReceiptListener(Listener):
     def __init__(self, timeout):
