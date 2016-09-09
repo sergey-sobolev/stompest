6eb1dc20ec75f1e481c7565d1e51751f07d41be8
Jan Müller <nikipore@gmail.com>
2016-09-09 21:55:32 +0200

    Bump version to 2.2.1

--- src/async/stompest/async/__init__.py
+++ src/async/stompest/async/__init__.py
@@ -1,4 +1,4 @@
 VERSION = '2.2'
-FULL_VERSION = '2.2.0'
+FULL_VERSION = '2.2.1'
 
 from stompest.async.client import Stomp
--- src/core/stompest/sync/__init__.py
+++ src/core/stompest/sync/__init__.py
@@ -1,4 +1,4 @@
 VERSION = '2.2'
-FULL_VERSION = '2.2.0'
+FULL_VERSION = '2.2.1'
 
 from stompest.sync.client import Stomp
