c96a461ccef18cf4736a6f86aff038128fe68bcb
Caleb Hattingh <caleb.hattingh@gmail.com>
2017-03-19 16:18:43 +1000

    Unfortunately, extras_require = {'tls': 'twisted[tls]'} doesn't work :((

--- src/async/setup.py
+++ src/async/setup.py
@@ -29,12 +29,9 @@ setup(
     zip_safe=True,
     install_requires=[
         'stompest==%s' % FULL_VERSION
-        , 'twisted>=16.4.0'
+        , 'twisted[tls] >= 16.4.0'
     ],
-    tests_require=(['mock'] if sys.version_info[0] == 2 else []) + [
-        'pyopenssl'
-        , 'service_identity'
-    ],
+    tests_require=['mock'] if sys.version_info[0] == 2 else [],
     test_suite='stompest.async.tests',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
